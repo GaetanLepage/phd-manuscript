fb86cfce4a09a590a20edf1e1c1883e6595d4e6c
Gaetan Lepage <gaetan@glepage.com>
2025-10-21 21:48:29 +0200

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    typst
+    liberation-sans-narrow
+  ];
+
+  env = {
+    TYPST_FONT_PATHS =
+      let
+        typst-fonts = pkgs.symlinkJoin {
+          name = "typst-fonts";
+          paths = with pkgs; [
+            liberation-sans-narrow
+          ];
+        };
+      in
+      "${typst-fonts}/share/fonts";
+  };
+}
