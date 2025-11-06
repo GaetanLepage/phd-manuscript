0a9fdad191d450de59bb66c16c97f0808a70f178
Gaetan Lepage <gaetan@glepage.com>
2025-11-06 13:32:51 +0100

    use flake-parts

--- flake.nix
+++ flake.nix
@@ -5,19 +5,26 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     # Last revision with Typst 0.13.1
     # nixpkgs.url = "github:nixos/nixpkgs/01f116e4df6a15f4ccdffb1bcd41096869fb385c";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      devShells.${system}.default = import ./shell.nix {
-        pkgs = import nixpkgs { inherit system; };
-      };
+    { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        };
     };
 }
