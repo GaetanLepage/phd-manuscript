88f60aa170f3fdca882ad6895309ec77d28ecc1a
Gaetan Lepage <gaetan@glepage.com>
2025-11-25 14:48:03 +0100

    use Typst 0.13.1 to get a valid PDF file

--- shell.nix
+++ shell.nix
@@ -13,7 +13,32 @@ in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     tinymist
-    typst
+    (typst.overrideAttrs (old: rec {
+      version = "0.13.1";
+      src = pkgs.fetchFromGitHub {
+        owner = "typst";
+        repo = "typst";
+        tag = "v${version}";
+        hash = "sha256-SGFD6KhBoFFL9mBS3Pdid7exDhWCfieLiFpt1C4SWHo=";
+        leaveDotGit = true;
+        postFetch = ''
+          cd $out
+          git rev-parse HEAD > COMMIT
+          rm -rf .git
+        '';
+      };
+      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+        inherit src;
+        inherit (old) pname version;
+        hash = "sha256-4kVj2BODEFjLcrh5sxfcgsdLF2Zd3K1GnhA4DEz1Nl4=";
+      };
+      doCheck = false;
+      doInstallCheck = false;
+      postPatch = ''
+        substituteInPlace tests/src/tests.rs --replace-fail 'ARGS.num_threads' 'ARGS.test_threads'
+        substituteInPlace tests/src/args.rs --replace-fail 'num_threads' 'test_threads'
+      '';
+    }))
     typstyle
   ];
 
